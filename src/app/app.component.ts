165065364aa2943238108a84ea49fac8a06e09f9
ChaseSong <Chasesong@anda.network>
2018-10-23 18:34:01 +0800

    submit code

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gridAreas';
+
+  // country = ['a', 'b', 'c', 'd'];
+  country = 'a';
+  phone = '';
+
+
+  termsView(){
+
+  }
+
+
+
+}
